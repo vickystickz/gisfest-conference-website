513011a697087de8b709d3c19b699e34c50be62e
jeafreezy <jolaiyaemmanuel@gmail.com>
2023-05-08 16:59:29 +0100

    linkified logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,16 @@
 import ConferenceEdition from '@/components/ConferenceEdition';
 import GISfestIcon from '@/components/icons/GISfestIcon';
 import { ConferenceEditions } from '@/config/editions';
+import Link from 'next/link';
 
 export default function Home() {
 	return (
 		<>
 			<div className="flex wrapper  p-10 w-full flex-col gap-20 items-center justify-center bg-fullscreenBgContour bg-right-bottom bg-no-repeat">
-				<div className="p-6 rounded-full shadow-primary">
-					<GISfestIcon />
+				<div className="p-6 rounded-full shadow-primary cursor-pointer">
+					<Link href="/">
+						<GISfestIcon />
+					</Link>
 				</div>
 				{ConferenceEditions.map((edition, id) => (
 					<ConferenceEdition
