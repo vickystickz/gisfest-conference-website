257e1b70ec9aa0f7bc0c8baa1a5a39ec54247741
vickystickz <kademvicky@gmail.com>
2023-05-21 11:51:44 +0100

    Updated mobile menu link click

--- components/jumbotron/Jumbotron.tsx
+++ components/jumbotron/Jumbotron.tsx
@@ -12,7 +12,6 @@ const Jumbotron = () => {
         <Image src={jumbotron.heroImage} alt='hero-image' className='top-0 object-cover bottom-0 z-0 absolute h-full w-full' />
         <div className='bg-gradient  absolute z-10 top-0 w-full h-full'>
         </div>
-         
          <HeroContent conference={jumbotron.conference} buttons={jumbotron.heroButtons}  />
     </div>
     
