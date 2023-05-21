f1ada21cce658f2101512bed583e639fbe2dee4b
vickystickz <kademvicky@gmail.com>
2023-05-21 12:50:02 +0100

    Updated styling for hero contents

--- components/jumbotron/HeroContent.tsx
+++ components/jumbotron/HeroContent.tsx
@@ -9,13 +9,13 @@ type THeroContent = {
 
 const HeroContent = ({ conference, buttons }: THeroContent) => {
   return (
-    <div className="z-20 flex-grow flex flex-col laptop:gap-24 gap-14 tablet:gap-12  w-full relative h-full laptop:mt-[3%] tablet:mt-[3%] mt-[10%] mb-[60%] px-6 laptop:px-24 tablet:px-14 max-w-screen-tv mx-auto">
+    <div className="z-20 flex-grow flex flex-col laptop:gap-24 gap-14 tablet:gap-12  w-full relative h-full laptop:mt-[3%] tablet:mt-[3%] tablet:mb-0  laptop:mb-0 mt-[10%] mb-[60%] px-6 laptop:px-24 tablet:px-14 max-w-screen-tv mx-auto">
       <div className="flex flex-col laptop:gap-[2rem] tablet:gap-[2rem] gap-2">
         <div className="flex gap-8">
           <HeroText text={`GISfest ${conference.year}`} />
           <HeroText text={conference.edition} />
         </div>
-        <h1 className="laptop:text-h1 tablet:text-h2 text-h4 laptop:leading-[92px] laptop:max-w-[770px] tablet:w-[70%] w-[90%] text-white font-clashDisplay laptop:font-p-semibold font-p-semibold ">
+        <h1 className="laptop:text-h1 tablet:text-h2 text-h4 laptop:leading-[92px] laptop:max-w-[770px] tablet:w-[70%] max-w-[300px] min-w-[70%] text-white font-clashDisplay laptop:font-p-semibold font-p-semibold ">
           {conference.title}
         </h1>
         <div className="flex flex-col laptop:flex-row tablet:flex-row laptop:gap-8 gap-4 laptop:p-0 tablet:p-0 pb-[32px] pt-5">
